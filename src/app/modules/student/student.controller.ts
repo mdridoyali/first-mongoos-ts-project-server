25e0245008f2a0f20497154d1ef47e1cc5443c14
mdridoyali <mdridoyislamr@gmail.com>
2024-06-03 21:14:34 +0600

    Done create student feature

---
+++ src/app/modules/student/student.controller.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from 'express';
+import { StudentServices } from './student.service';
+
+const createStudent = async (req: Request, res: Response) => {
+  try {
+    const Student = req.body.student;
+    const result = await StudentServices.createStudentIntoDB(Student);
+    res.status(200).json({
+      success: true,
+      message: 'Student is created successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentsFormDb();
+    res.status(200).json({
+      success: true,
+      message: 'Student are retrieved successfully',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getSingleStudent = async (req: Request, res: Response) => {
+  try {
+    const studentId = req.params.studentId;
+    const result = await StudentServices.getSingleStudentFormDb(studentId);
+    res.status(200).json({
+      success: true,
+      message: 'Student is retrieved successfully',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const StudentController = {
+  createStudent,
+  getAllStudents,
+  getSingleStudent,
+};
--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,15 @@
-import mongoose from "mongoose";
-import config from "./app/config";
-import app from "./app";
+import mongoose from 'mongoose';
+import config from './app/config';
+import app from './app';
 
 async function main() {
-    try {
-        await mongoose.connect(config.database_url as string);
-
-        app.listen(config.port, () => {
-            console.log(`Example app listening on port ${config.port}`)
-        })
-    }
-    catch (error) {
-        console.log(error);
-    }
+  try {
+    await mongoose.connect(config.database_url as string);
+    app.listen(config.port, () => {
+      console.log(`app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
-
-main()
-
-
-
-
-
-
+main();
